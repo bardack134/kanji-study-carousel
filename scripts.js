eb09f3404886e413427564d6016289bcdf3f6140
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-11-29 06:04:13 +0900

    random boton ok

--- scripts.js
+++ scripts.js
@@ -22,13 +22,24 @@ const kanji = [
       img: "https://th.bing.com/th/id/R.bb363678f5b48fcf0f92f9100fcb380f?rik=1XVTfPnaAfJkQg&riu=http%3a%2f%2fikanji.jp%2fuser_data%2fimages%2fupload%2fcharacter%2foriginal%2fE6958F.png&ehk=jOadEwFq4HoA3Ibr%2bGZWV182KjRE9rE97QUu4V%2fwqR8%3d&risl=&pid=ImgRaw&r=0",
       example: "敏感、過敏、機敏、敏速",
     },
+    
     {
-      meaning: "Bob Brown",
-      reading: "Marketing Specialist",
-      img: "https://via.placeholder.com/100",
-      text: "Bob's marketing strategies brought excellent results.",
+      meaning: "Abundant, rich",
+      kunyomi: " ",
+      onyomi: "ゆう",
+      img: "https://th.bing.com/th/id/OIP.cuRAPeMQCX6q94SCKWGXbgAAAA?rs=1&pid=ImgDetMain",
+      example: "余裕、裕福、富裕",
     },
+    {
+      meaning: "Just so, Absolutely",
+      kunyomi: "これ、この、ここ",
+      onyomi: "ぜ、シ",
+      img: "https://kanji.jitenon.jp/shotai2/1159.gif",
+      example: "是非とも、是正、是認",
+    },
+    
   ];
+
 
 
 // 手順 2： DOM要素を取
@@ -54,10 +65,10 @@ function showInformation(index) {
     kunyomi.innerHTML=item.kunyomi;
     imagen.src=item.img;
     
-    meaning.innerHTML=item.meaning;
+    meaning .innerHTML=item.meaning;
     example.innerHTML=item.example;
     
-}
+};
 
 /* NOTE: ページが読み込まれたとき (DOMContentLoaded イベント)、最初のレビューを表示します。
  ページが完全にロードされる前にスクリプトを実行したい場合に便利です。
@@ -66,18 +77,41 @@ function showInformation(index) {
 document.addEventListener("DOMContentLoaded", function(){
     console.log('HTML が読み込まれました！'); //デバッグのために
     showInformation(index);
-})
+});
 
 
 // 手順　５：「次へ」ボタンをクリックしたときの動作
 function goRight(){
     index +=1;
-    showInformation(index);
-}
+       
+    if (index>kanji.length-1) {
+        console.log('現在のインデックスが JSON 形式の長さを超えています');   
+        index =0;
+        showInformation(index);
+
+    }else{
+        showInformation(index);
+    }
+    console.log(index); 
+};
  
 // 手順　6：「前へ」ボタンをクリックしたときの動作
 function goLeft(){
+     
     index -=1;
+    console.log(index);     
+    if (index<0) {
+        index =kanji.length-1;
+        showInformation(index);
+        
+    }else{
+        console.log('go left');   
+        showInformation(index);
+    }
+};
+
+function randomKanji(){
+    index=Math.floor(Math.random() * kanji.length);
+    console.log(index);
     showInformation(index);
 }
-  
